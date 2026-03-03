495e23df8108bdc0766aa17126bf54b3f24afb98
Borin Ouch <borin.ouch@gmail.com>
2026-03-02 22:34:08 -0800

    Add HelloTriangleApplication skeleton

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,62 @@ namespace {
   const int kWindowHeight = 600;
 }
 
+
+class HelloTriangleApplication {
+public:
+  void Run() {
+    InitVulkan();
+    MainLoop();
+    Cleanup();
+  }
+
+private:
+  void InitVulkan() {
+  }
+
+  void MainLoop() {
+  }
+
+  void Cleanup() {
+  }
+};
+
+
 auto main(int argc, char* argv[]) -> int {
-  std::println("Hello, Vulkan");
 
-  if (!glfwInit()) {
-    std::println(stderr, "Failed to initialize GLFW");
+  try {
+    HelloTriangleApplication app;
+    app.Run();
+  } catch (const std::exception& e) {
+    std::println(stderr, "Error: {}", e.what());
     return 1;
   }
 
-  GLFWwindow* window = glfwCreateWindow(kWindowWidth, kWindowHeight, kWindowTitle, nullptr, nullptr);
-  if (!window) {
-    std::println(stderr, "Failed to create window");
-    glfwTerminate();
-    return 1;
-  }
+  // std::println("Hello, Vulkan");
 
-  glfwMakeContextCurrent(window);
+  // if (!glfwInit()) {
+  //   std::println(stderr, "Failed to initialize GLFW");
+  //   return 1;
+  // }
 
-  while (!glfwWindowShouldClose(window)) {
+  // GLFWwindow* window = glfwCreateWindow(kWindowWidth, kWindowHeight, kWindowTitle, nullptr, nullptr);
+  // if (!window) {
+  //   std::println(stderr, "Failed to create window");
+  //   glfwTerminate();
+  //   return 1;
+  // }
 
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+  // glfwMakeContextCurrent(window);
 
-  glfwTerminate();
+  // while (!glfwWindowShouldClose(window)) {
 
-  std::println("Terminating...");
+  //   glfwSwapBuffers(window);
+  //   glfwPollEvents();
+  // }
+
+  // glfwTerminate();
+
+  // std::println("Terminating...");
 
   return 0;
 }

ae8db28f1d4a772fe5cb04e76edb3b268095349d
Borin Ouch <borin.ouch@gmail.com>
2026-03-02 21:32:54 -0800

    Initialize GLFW, create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <print>
+#include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+
+namespace {
+  const char* const kWindowTitle = "Learning Vulkan";
+  const int kWindowWidth = 800;
+  const int kWindowHeight = 600;
+}
 
 auto main(int argc, char* argv[]) -> int {
   std::println("Hello, Vulkan");
+
+  if (!glfwInit()) {
+    std::println(stderr, "Failed to initialize GLFW");
+    return 1;
+  }
+
+  GLFWwindow* window = glfwCreateWindow(kWindowWidth, kWindowHeight, kWindowTitle, nullptr, nullptr);
+  if (!window) {
+    std::println(stderr, "Failed to create window");
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  std::println("Terminating...");
+
   return 0;
 }

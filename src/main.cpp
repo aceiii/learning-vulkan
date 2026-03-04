18e1e1e96865f3bdcde5cddae932367c2ac87f4a
Borin Ouch <borin.ouch@gmail.com>
2026-03-03 19:01:39 -0800

    Init glfw, and create window for vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,49 @@
 #include <print>
+#include <vulkan/vulkan_raii.hpp>
+
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
-#include <vulkan/vulkan.hpp>
-
 namespace {
-  const char* const kWindowTitle = "Learning Vulkan";
-  const int kWindowWidth = 800;
-  const int kWindowHeight = 600;
+  constexpr char const* kWindowTitle = "Learning Vulkan";
+  constexpr int kWindowWidth = 800;
+  constexpr int kWindowHeight = 600;
 }
 
 
 class HelloTriangleApplication {
 public:
   void Run() {
+    InitWindow();
     InitVulkan();
     MainLoop();
     Cleanup();
   }
 
 private:
+  void InitWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window_ = glfwCreateWindow(kWindowWidth, kWindowHeight, kWindowTitle, nullptr, nullptr);
+  }
+
   void InitVulkan() {
   }
 
   void MainLoop() {
+    while (!glfwWindowShouldClose(window_)) {
+      glfwPollEvents();
+    }
   }
 
   void Cleanup() {
+    glfwDestroyWindow(window_);
+    glfwTerminate();
   }
+
+  GLFWwindow* window_;
 };
 
 
